6ab7b94535e9e0d459b97cb1b35e32a1b1abfb22
wHo69 <hu_bowen@students.edu.sg>
2023-04-17 11:46:22 +0800

    Add comments

--- Week 1/TotalCost.py
+++ Week 1/TotalCost.py
@@ -1,7 +1,9 @@
 # Hu Bowen, S10255800B
 
+# Prompt user for price and GST percentage
 price = float(input("Enter the price of the item: "))
 gst = float(input("Enter the GST's percentage value (e.g. 8% -> 8): "))
 
+# Calculate total cost
 total = price * (1 + (gst / 100))
 print("$" + str(total))
